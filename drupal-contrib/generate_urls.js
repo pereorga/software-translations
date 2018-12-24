d6eb4b99de73fce230e32dd7dff63ebd52062ae4
Pere Orga <pere@orga.cat>
2018-12-25 00:10:19 +0100

    drupal-contrib: Generate urls async

--- drupal-contrib/generate_urls.js
+++ drupal-contrib/generate_urls.js
@@ -1,32 +1,43 @@
 const request = require('request');
 const cheerio = require('cheerio');
-var fs = require('fs')
+const fs      = require('fs')
+const async   = require('async');
 
-function generatePoUlrs(version) {
 
-  var prefix_url = 'https://ftp-origin.drupal.org/files/translations/' + version + '/';
+var q = async.queue(function(project, callback) {
+  printPoUrls(project);
+  callback();
+}, 10);
+
+
+function printPoUrls(project) {
+  request(project, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      $('a').each(function() {
+        if ($(this).attr('href').indexOf('.ca.po') !== -1) {
+          var po_file_url = project + $(this).attr('href');
+          console.log(po_file_url);
+        }
+      });
+    }
+    else {
+      console.error(error + " with url " + project);
+    }
+  });
+}
+
+
+function generatePoUlrs(branch) {
+
+  var prefix_url = 'https://ftp-origin.drupal.org/files/translations/' + branch + '/';
 
   request(prefix_url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
 
       var $ = cheerio.load(body);
-
       $('a').each(function() {
-        var project_path = prefix_url + $(this).attr('href');
-        request(project_path, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var $p = cheerio.load(body);
-            $p('a').each(function() {
-              if ($p(this).attr('href').indexOf('.ca.po') !== -1) {
-                var po_file_url = project_path + $p(this).attr('href');
-                fs.appendFileSync('urls_' + version + '.txt', po_file_url + '\n');
-              }
-            });
-          }
-          else {
-            console.error("Error " + error + " in " + project_path);
-          }
-        });
+        q.push(prefix_url + $(this).attr('href'));
       });
     }
     else {
@@ -34,6 +45,8 @@ function generatePoUlrs(version) {
     }
   });
 }
+
+
 
 if (typeof process.argv[2] === 'undefined' || (
     process.argv[2] !== '6.x'
@@ -43,11 +56,5 @@ if (typeof process.argv[2] === 'undefined' || (
   console.error('Usage: node generate_urls.js 7.x')
 }
 else {
-
-  var version = process.argv[2];
-
-  // Remove file contents.
-  fs.writeFileSync('urls_' + version + '.txt', '');
-
-  generatePoUlrs(version);
+  generatePoUlrs(process.argv[2]);
 }
