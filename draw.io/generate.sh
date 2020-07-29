dbbaf2122add1ed758182efe2d2b6120eb8eb657
Pere Orga <pere@orga.cat>
2020-07-29 22:44:06 +0200

    draw.io: update

--- draw.io/generate.sh
+++ draw.io/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl "https://docs.google.com/spreadsheets/d/1FoYdyEraEQuWofzbYCDPKN7EdKgS_2ZrsDrOA8scgwQ/export?exportFormat=csv" > draw1.csv
-csvcut draw1.csv -c 1,2,8 > draw2.csv
+csvcut draw1.csv -c 1,2,7 --skip-lines 2 > draw2.csv
 csv2po draw2.csv ca.po
 rm -f draw1.csv draw2.csv
