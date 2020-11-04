9cda6b039ddcc20b26faaab3ee4edb903724e768
Pere Orga <pere@orga.cat>
2020-11-04 20:16:19 +0100

    draw.io: follow redirect in curl

--- draw.io/generate.sh
+++ draw.io/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-curl "https://docs.google.com/spreadsheets/d/1FoYdyEraEQuWofzbYCDPKN7EdKgS_2ZrsDrOA8scgwQ/export?exportFormat=csv" > draw1.csv
+curl -L "https://docs.google.com/spreadsheets/d/1FoYdyEraEQuWofzbYCDPKN7EdKgS_2ZrsDrOA8scgwQ/export?exportFormat=csv" > draw1.csv
 csvcut draw1.csv -c 1,2,7 --skip-lines 2 > draw2.csv
 csv2po draw2.csv ca.po
 rm -f draw1.csv draw2.csv
