99b0e0a6363200a5e3c74ed82da9b4608f14341b
Pere Orga <pere@orga.cat>
2022-12-29 23:32:46 +0100

    muCommander: fix encoding

--- mucommander/generate.sh
+++ mucommander/generate.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 
 curl "https://raw.githubusercontent.com/mucommander/mucommander/master/mucommander-translator/src/main/resources/dictionary.properties" > dictionary.properties
 curl "https://raw.githubusercontent.com/mucommander/mucommander/master/mucommander-translator/src/main/resources/dictionary_ca.properties" > dictionary_ca.properties
-prop2po -i dictionary_ca.properties -o dictionary_ca.po --template=dictionary.properties
+prop2po --encoding=UTF-8 -i dictionary_ca.properties -o dictionary_ca.po --template=dictionary.properties
