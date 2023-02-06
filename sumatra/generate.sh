dac536fbc8ab774282077a681f18c00a6dd10cde
Pere Orga <pere@orga.cat>
2023-02-06 21:35:10 +0100

    sumatra: fix PO generation

--- sumatra/generate.sh
+++ sumatra/generate.sh
@@ -2,6 +2,12 @@
 
 cd "$(dirname "$0")"
 
+#. extracted from rawtherapee.csv
+echo "msgid \"\"" > sumatra.po
+echo "msgstr \"\"" >> sumatra.po
+echo "\"Content-Type: text/plain; charset=UTF-8\n\""  >> sumatra.po
+echo ""  >> sumatra.po
+
 curl "https://raw.githubusercontent.com/sumatrapdfreader/sumatrapdf/master/src/docs/translations.txt" > translations.txt
 
 # Extract English and Catalan
@@ -10,7 +16,7 @@ grep -E '^(\:|ca\:)' translations.txt > translations_ca.txt
 # Convert text file to PO
 sed 's/^:/msgid "/' translations_ca.txt > translations_po_step1.txt
 sed 's/^ca:/msgstr "/' translations_po_step1.txt > translations_po_step2.txt
-sed 's/$/"/' translations_po_step2.txt > sumatra.po
+sed 's/$/"/' translations_po_step2.txt >> sumatra.po
 
 if [[ -z $(msgattrib sumatra.po 2> /dev/null) ]]; then
     msgattrib sumatra.po
