7e2b0e471113c76cfb9b340d17bb09a6643333b6
Pere Orga <pere@orga.cat>
2023-06-22 17:59:32 +0200

    mucommander: delete outdated project

--- mucommander/generate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-
-curl "https://raw.githubusercontent.com/mucommander/mucommander/master/mucommander-translator/src/main/resources/dictionary.properties" > dictionary.properties
-curl "https://raw.githubusercontent.com/mucommander/mucommander/master/mucommander-translator/src/main/resources/dictionary_ca.properties" > dictionary_ca.properties
-prop2po --encoding=UTF-8 -i dictionary_ca.properties -o dictionary_ca.po --template=dictionary.properties
