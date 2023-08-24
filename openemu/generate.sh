8579d2af981b3f51ca44a85d92bc2da20efe30f0
Pere Orga <pere@orga.cat>
2023-08-24 19:11:17 +0200

    refactor(openemu): simplify script

--- openemu/generate.sh
+++ openemu/generate.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
+
 cd "$(dirname "$0")"
 
-base_url="https://raw.githubusercontent.com/OpenEmu/OpenEmu/master/OpenEmu"
-declare -a files=("ControlLabels" "Debug" "InfoPlist" "Localizable" "MainMenu" "OEControls")
+repo_url="https://github.com/OpenEmu/OpenEmu.git"
+temp_dir="temp_openemu"
 
-# Fetching and filtering ca.lproj files
-for file in "${files[@]}"; do
-    curl "$base_url/ca.lproj/$file.strings" | grep -F -v '// TODO' | grep -F -v '/*' | grep . > "files/$file.strings"
-done
+rm -rf "$temp_dir"
+git clone --depth 1 "$repo_url" "$temp_dir"
 
-# Fetching en.lproj files
+files=($(cd "$temp_dir/OpenEmu/ca.lproj" && ls *.strings | sed 's/.strings$//'))
 for file in "${files[@]}"; do
-    curl "$base_url/en.lproj/$file.strings" > "files/$file.template"
-done
-
-# Running prop2po
-for file in "${files[@]}"; do
+    cp "$temp_dir/OpenEmu/en.lproj/$file.strings" "files/$file.template"
+    cat "$temp_dir/OpenEmu/ca.lproj/$file.strings" | grep -F -v '// TODO' | grep -F -v '/*' | grep . > "files/$file.strings"
     prop2po --personality=strings --encoding=utf-8 -i "files/$file.strings" -o "po/$file.po" --template="files/$file.template"
 done
