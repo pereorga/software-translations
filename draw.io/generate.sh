c6374c99b11627c03f3e7cafd32ea90a3c17a395
Pere Orga <pere@orga.cat>
2025-04-08 21:28:21 +0200

    fix(drawio): download correct tab

--- draw.io/generate.sh
+++ draw.io/generate.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 rm -rf tmp/
 mkdir tmp
 
-curl -L "https://docs.google.com/spreadsheets/d/1FoYdyEraEQuWofzbYCDPKN7EdKgS_2ZrsDrOA8scgwQ/export?exportFormat=csv" > tmp/draw1.csv
+curl -L "https://docs.google.com/spreadsheets/d/1FoYdyEraEQuWofzbYCDPKN7EdKgS_2ZrsDrOA8scgwQ/export?format=csv&gid=0" > tmp/draw1.csv
 csvcut tmp/draw1.csv -c 1,2,7 --skip-lines 2 > tmp/draw2.csv
 csv2po tmp/draw2.csv ca.po
